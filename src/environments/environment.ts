2e1b9ecf3ddd5110dcf41dc893acc25947aca3a2
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-08-08 19:33:19 +0300

    Firstcommit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'stajhero',
+    appId: '1:723213947620:web:a04b2f02a210dbfafb12f1',
+    storageBucket: 'stajhero.appspot.com',
+    apiKey: 'AIzaSyBQDeRCXARIShr629GIngUW9HgFc5FxxcA',
+    authDomain: 'stajhero.firebaseapp.com',
+    messagingSenderId: '723213947620',
+  },
+};
